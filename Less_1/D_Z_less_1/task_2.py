b13e95ed794e8d51742098bfdd142fe3d49d6c38
xartiou <xartiou@gmail.com>
2022-02-26 09:19:18 +0300

    task_2 вариант с f string

--- Less_1/D_Z_less_1/task_2.py
+++ Less_1/D_Z_less_1/task_2.py
@@ -4,14 +4,31 @@
 # (т.е. ни в коем случае не используя методы encode, decode или функцию bytes)
 # и определить тип, содержимое и длину соответствующих переменных.
 
-word_1 = 'b' + "'class'"
-word_2 = 'b' + "'function'"
-word_3 = 'b' + "'method'"
+# вариант с конкатинацией
+# word_1 = 'b' + "'class'"
+# word_2 = 'b' + "'function'"
+# word_3 = 'b' + "'method'"
+#
+# words = []
+# words.append(eval(word_1))
+# words.append(eval(word_2))
+# words.append(eval(word_3))
+#
+# for word in words:
+#     print(word,type(word),len(word))
 
-words = []
-words.append(eval(word_1))
-words.append(eval(word_2))
-words.append(eval(word_3))
+# вариант с f string
+
+word_1 = 'class'
+word_2 = 'function'
+word_3 = 'method'
+
+words = [word_1, word_2, word_3]
 
 for word in words:
-    print(word,type(word),len(word))
+    w = eval(f"b'{word}'")
+    print('-' * 50)
+    print('type: ', type(w))
+    print(w)
+    print('length of variable in bites: ', len(w))
+
