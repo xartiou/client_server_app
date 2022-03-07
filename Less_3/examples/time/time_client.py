a335076f9f8589fa4c35ab55aab224e6c71d2f06
xartiou <xartiou@gmail.com>
2022-03-07 19:02:40 +0300

    port 8800

--- Less_3/examples/time/time_client.py
+++ Less_3/examples/time/time_client.py
@@ -1,9 +1,9 @@
-"""programm client time"""
+"""Program client time"""
 
 from socket import socket, AF_INET, SOCK_STREAM
 
-CLIENT_SOCK = socket(AF_INET, SOCK_STREAM)
-CLIENT_SOCK.connect(('localhost', 8888))  # поключаемся к серверу
+CLIENT_SOCK = socket(AF_INET, SOCK_STREAM)   # создаём сокет
+CLIENT_SOCK.connect(('localhost', 8800))  # подключаемся к серверу
 TIME_BYTES = CLIENT_SOCK.recv(1024)  # ждём ответа от сервера
-print(f"Текущее время {TIME_BYTES.decode('utf-8')}" )
+print(f"Текущее время {TIME_BYTES.decode('utf-8')}")
 CLIENT_SOCK.close()
