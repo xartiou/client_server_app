cf7710f83c172bad15ca265a8a03c82771f24968
xartiou <xartiou@gmail.com>
2022-03-07 14:59:58 +0300

    rename folder

---
+++ Less_3/examples/data/data_server.py
@@ -0,0 +1,19 @@
+"""Программа для получения приветствия от клиента на сервер"""
+
+from  socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
+
+SERV_SOCK = socket(AF_INET, SOCK_STREAM)
+SERV_SOCK.setsockopt((SOL_SOCKET, SO_REUSEADDR, 1))
+SERV_SOCK.bind(('', 8007))
+SERV_SOCK.listen()
+
+try:
+    while True:
+        CLIENT_SOCK, ADDR = SERV_SOCK.accept()
+        DATA = CLIENT_SOCK.recv(4096)
+        print(f"Сообщение: {DATA.decode('utf-8')} было отправлено клиенту")
+        MSG = 'Привет, клиент'
+        CLIENT_SOCK.send(MSG.encode('utf-8'))
+        CLIENT_SOCK.close()
+finally:
+    SERV_SOCK.close()
