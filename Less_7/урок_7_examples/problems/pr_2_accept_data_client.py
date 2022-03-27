c592440b5ba7a8bd1d2052d710fa44d13a9a8181
xartiou <xartiou@gmail.com>
2022-03-27 16:17:15 +0300

    Урок 7. problem accept

---
+++ Less_7/урок_7_examples/problems/pr_2_accept_data_client.py
@@ -0,0 +1,13 @@
+"""Программа клиента для отправки приветствия на сервер"""
+
+from socket import socket, AF_INET, SOCK_STREAM
+
+CLIENT_SOCK = socket(AF_INET, SOCK_STREAM)
+CLIENT_SOCK.connect(('localhost', 8007))
+while True:
+    MSG = 'Привет, сервер!'
+    CLIENT_SOCK.send(MSG.encode('utf-8'))
+    DATA = CLIENT_SOCK.recv(4096)
+    print(f"Сообщение от сервера: {DATA.decode('utf-8')} длиной {len(DATA)}")
+
+CLIENT_SOCK.close()
